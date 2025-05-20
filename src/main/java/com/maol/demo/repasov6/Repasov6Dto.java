7d7df9b89461c37c05a68d41f0e08d86d2741e0f
angelesortega <mariaortega5l@outlook.com>
2025-05-20 08:35:23 +0200

    clases repasov6

--- src/main/java/com/maol/demo/repasov6/Repasov6Dto.java
+++ src/main/java/com/maol/demo/repasov6/Repasov6Dto.java
@@ -2,4 +2,15 @@ package com.maol.demo.repasov6;
 
 public class Repasov6Dto {
 
+	private String nombre = "angeles";
+	private String primerApellido = "ortega";
+	private int edad = 37;
+	private double altura = 1.62;
+	private boolean haceEjercercicio = true;
+
+
+
+
+
+
 }
